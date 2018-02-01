7ec587cee043ea18f53616a04d837ac38c0ff6c1
PortPiratech1 <portpiratech@gmail.com>
2018-01-31 20:56:27 -0600

    Grabber speed determined

--- TestBot/src/org/usfirst/frc/team4804/robot/Robot.java
+++ TestBot/src/org/usfirst/frc/team4804/robot/Robot.java
@@ -7,6 +7,7 @@ import org.usfirst.frc.team4804.robot.subsystems.Grabber;
 import org.usfirst.frc.team4804.robot.subsystems.LinearActuator;
 import org.usfirst.frc.team4804.robot.subsystems.SolenoidSubsystem;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -77,6 +78,17 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousInit() {
+		//gameData is a string relative to the alliance color; "ABC" where A = closest switch, B = scale, C = furthest switch
+		//A, B, C are either L (left) or R (right)
+		String gameData;
+		gameData = DriverStation.getInstance().getGameSpecificMessage();
+		if(gameData.charAt(0) == 'L')
+		{
+			//Put left auto code here
+		} else {
+			//Put right auto code here
+		}
+		
 		autonomousCommand = chooser.getSelected();
 
 		/*
--- TestBot/src/org/usfirst/frc/team4804/robot/subsystems/Grabber.java
+++ TestBot/src/org/usfirst/frc/team4804/robot/subsystems/Grabber.java
@@ -5,7 +5,11 @@ import org.usfirst.frc.team4804.robot.RobotMap;
 import org.usfirst.frc.team4804.robot.SeatMotor;
 import org.usfirst.frc.team4804.robot.commands.Grab;
 
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.AnalogTrigger;
+import edu.wpi.first.wpilibj.AnalogTriggerOutput;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.AnalogTriggerOutput.AnalogTriggerType;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -14,14 +18,24 @@ public class Grabber extends Subsystem {
 	//This is a very basic version of grabber's program
 	
 	private SeatMotor grabberMotor;
-	private AnalogTrigger grabberAnalogEncoder;
+	private AnalogInput grabberAnalogInput;
+	private AnalogTrigger grabberAnalogTrigger;
+	private AnalogTriggerOutput grabberAnalogTriggerOutput;
 	private int count;
 	private boolean encState;
+	private long lastTime;
+	
+	//Encoder is 174.9:1 gear ratio (174.9 counts per revolution)
+	//(50ish reads per second)/(174.9 counts) = 0.285 speed necessary for full encoder resolution
+	private double speedMult = 0.28;
 	
 	public Grabber(){
 		grabberMotor = new SeatMotor(RobotMap.grabberMotorId);
-		grabberAnalogEncoder = new AnalogTrigger(RobotMap.grabberEncoderId);
-		grabberAnalogEncoder.setLimitsVoltage(3.5, 5.0);
+		//grabberAnalogInput = new AnalogInput(RobotMap.grabberEncoderId);
+		//grabberAnalogTrigger = new AnalogTrigger(grabberAnalogInput);
+		grabberAnalogTrigger = new AnalogTrigger(RobotMap.grabberEncoderId);
+		grabberAnalogTrigger.setLimitsVoltage(3.0, 3.75);
+		//grabberAnalogTriggerOutput = new AnalogTriggerOutput(grabberAnalogTrigger, AnalogTriggerType.kRisingPulse);
 		
 		count = 0;
 		encState = getEncoderState();
@@ -30,6 +44,7 @@ public class Grabber extends Subsystem {
 		grabberMotor.getMotor().configPeakCurrentLimit(0, 0);
 		grabberMotor.getMotor().configContinuousCurrentLimit(1, 500);
 		
+		lastTime = System.currentTimeMillis();
 	}
 	
 	public void initDefaultCommand() {
@@ -38,34 +53,69 @@ public class Grabber extends Subsystem {
     }
 	
 	public void close() {
-    	double clamp = OI.operatorController.getY(Hand.kLeft);
-    	grabberMotor.setSpeed(clamp);
-    	count();
+		count();
+		smartDashboardOutput();
+		
+		
+		
+    	//double clampSpeed = -OI.operatorController.getY(Hand.kLeft);
     	
-    	SmartDashboard.putNumber("Seat motor encoder index", grabberAnalogEncoder.getIndex());
+    	//Encoder is 174.9:1 gear ratio (174.9 counts per revolution)
+    	//(50ish reads per second)/(174.9 counts) = 0.285 speed necessary for full encoder resolution
+    	/*grabberMotor.setSpeed(0.28 * clampSpeed);*/
+    	
+    	
+    	//Code to count the max number of pulses per second
+		/*count++;
+		SmartDashboard.putNumber("Seat motor encoder count test", count);
+		
+		if(System.currentTimeMillis() - lastTime > 1000) {
+			SmartDashboard.putNumber("Highest count", count);
+			count = 0;
+			lastTime = System.currentTimeMillis();
+		}*/
+	}
+
+	public void smartDashboardOutput() {
+		SmartDashboard.putNumber("Seat motor encoder index", grabberAnalogTrigger.getIndex());
     	SmartDashboard.putBoolean("Seat motor encoder bool", getEncoderState());
     	SmartDashboard.putNumber("Seat motor encoder count test", count);
     	SmartDashboard.putNumber("seat motor output current ", getMotorCurrent());
 	}
-
+	
 	public double getMotorCurrent() {
 		return grabberMotor.getMotor().getOutputCurrent();
 	}
 	
 	public boolean getEncoderState(){
-		 return grabberAnalogEncoder.getInWindow();
+		 return grabberAnalogTrigger.getTriggerState();
 	}
 	
-	public void count()  {
-		if(encState != getEncoderState() && getEncoderState() == true) { 
-			if(Math.signum(getMotorCurrent()) == 1.0) {
+	public double getMotorOutputPercentSignum() {
+		return Math.signum(grabberMotor.getMotor().getMotorOutputPercent());
+	}
+	
+	public void count() {
+		//SmartDashboard.putBoolean("Analog trigger output", grabberAnalogTriggerOutput.get());
+		/*if(grabberAnalogTriggerOutput.get()) {
+			if(getMotorOutputPercentSignum() == 1.0) {
 				count++;
-			} else if(Math.signum(getMotorCurrent()) == -1.0) {
+			} else if(getMotorOutputPercentSignum() == -1.0) {
+				count--;
+			}
+		}*/
+		
+		//Catches the rising edges
+		if(encState != getEncoderState() && getEncoderState() == true) { 
+			if(Math.signum(grabberMotor.getMotor().getMotorOutputPercent()) == 1) {
+				count++;
+			} else if(Math.signum(grabberMotor.getMotor().getMotorOutputPercent()) == -1) {
 				count--;
 			}
 		}
 		encState = getEncoderState();
-		//CONFUSED AND NOT WORKING SEND HELP
+		
+		//if it is supposed to have 175 pulses(i'm not entirely sure) it worked at ~1/4 speed
 	}
 	
 
