8ca182da6a2170c6e195415aa9aa599fc5d6252d
PortPiratech1 <portpiratech@gmail.com>
2018-02-10 09:45:37 -0600

    Added gyro and get gyro angle to the drive train.

--- TestBot/src/org/usfirst/frc/team4804/robot/commands/Drive.java
+++ TestBot/src/org/usfirst/frc/team4804/robot/commands/Drive.java
@@ -3,16 +3,20 @@ package org.usfirst.frc.team4804.robot.commands;
 import org.usfirst.frc.team4804.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
 public class Drive extends Command {
 
+	
+	
     public Drive() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.driveTrain);
+    
     }
 
     // Called just before this Command runs the first time
@@ -22,6 +26,7 @@ public class Drive extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.driveTrain.tankDrive();
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
